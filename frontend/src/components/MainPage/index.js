ce22ff3fa33d86f544c7a154d2c849914ba3251d
RostislavBartenev <rostislav.bartenev@gmail.com>
2020-10-14 21:49:04 +0600

    fixes

--- frontend/src/components/MainPage/index.js
+++ frontend/src/components/MainPage/index.js
@@ -1,18 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'; import OrganizationCard from '../OrganizationCard';
-import DepartmentCard from '../DepartmentCard';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+
+import './style.scss'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
 
 const MainPage = () => {
 
+
+  const classes = useStyles();
   const userInfo = useSelector(state => state.user)
   const isCreator = useSelector(state => state.aboutMe.isCreator)
   const orgArray = useSelector(state => state.organizations)
   const dep = useSelector(state => state.workerDeps)
 
   return (
-    <>
+    <div className="main-page">
       <h1>Главная страница</h1>
 
       {isCreator
@@ -23,18 +39,31 @@ const MainPage = () => {
           <p>Ваши организации:</p>
 
           <div className="profile__page">
-            {orgArray.length
-              ? <ul className="org-list ">
+            {orgArray.length ?
+              <>
                 {orgArray.map((org) => {
                   return (
                     <Link to={`/organization/${org._id}`} key={org._id}>
-                      <li className="org-list-task">
-                        <OrganizationCard {...org} />
-                      </li>
+                        <Card key={org._id} className={classes.root}>
+                          <CardActionArea>
+                            <CardMedia
+                              component="img"
+                              alt="Contemplative Reptile"
+                              height="180"
+                              image="https://static.tildacdn.com/tild3639-3835-4237-b964-623565623163/MoscowCityofficerentals.png"
+                              title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                              <Typography style={{marginTop: 10}} gutterBottom variant="h5" component="h2">
+                                {org.name}
+                              </Typography>
+                            </CardContent>
+                          </CardActionArea>
+                        </Card>
                     </Link>
                   )
                 })}
-              </ul>
+              </>
               : <p>Нет добавленных организаций</p>
             }
           </div>
@@ -49,15 +78,12 @@ const MainPage = () => {
               ?
               <>
                 <p> {userInfo.name + ' ' + userInfo.surname}, Вы являетесь сотрудником организации "{orgArray[0].name}"</p>
-                {/* ОТОБРАЖЕНИЕ ДЕПАРТМЕНТОВ С СЕРВЕРНЫХ ДАННЫХ (не из редакс!)
-                >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-                */}
                 <ul className="org-list ">
                   {dep.map((dep) => {
                     return (
                       <Link to={`/department/${dep._id}`} key={dep._id}>
                         <li className="dep-list-task">
-                          <DepartmentCard {...dep} />
+                          {dep.name}
                         </li>
                       </Link>
                     )
@@ -68,7 +94,7 @@ const MainPage = () => {
           }
         </>
       }
-    </>
+    </div>
   )
 }
 
