5d02cc8459d95894b7a9d992d9a22a38b6b860c4
RostislavBartenev <rostislav.bartenev@gmail.com>
2020-10-16 12:52:29 +0600

    done

--- backend/public/precache-manifest.3864b97b4ef086329c114c7956d4a6e7.js
+++ backend/public/precache-manifest.3864b97b4ef086329c114c7956d4a6e7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fed520fede4c0470e64956684b47a623",
+    "revision": "ccee6c413466a6de2ac1d2604b0c33f2",
     "url": "/index.html"
   },
   {
-    "revision": "715c64a825198ff10b82",
+    "revision": "ba8a33c709250b547267",
     "url": "/static/css/main.71aa39da.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f07e08ac.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "715c64a825198ff10b82",
-    "url": "/static/js/main.b6d2e7a5.chunk.js"
+    "revision": "ba8a33c709250b547267",
+    "url": "/static/js/main.671febf4.chunk.js"
   },
   {
     "revision": "2747b914e9a60db2276f",
